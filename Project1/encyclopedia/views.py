3f6a98003fb570f4bacd47f2e7b261fce92bf5c5
Bradlee Harrison <bradlee.harrison22@gmail.com>
2024-01-08 17:47:54 -0600

    Markdown formatting 1

--- Project1/encyclopedia/views.py
+++ Project1/encyclopedia/views.py
@@ -10,8 +10,29 @@ def index(request):
 
 
 def entry_view(request, entry_name):
-    print(util.get_entry(entry_name))
-    return render(request, f"encyclopedia/layout.html", {
-        "entries": util.get_entry(entry_name)
+    markdown = util.get_entry(entry_name)
+    lines = markdown.split('\n')
+
+    stack = []
+    for idx, line in enumerate(lines):
+        if line.startswith("#"):
+            header_level = len(line) - len(line.lstrip('#'))
+            header_content = line.lstrip('#').strip()
+            lines[idx] = f"<h{header_level}>{header_content}</h{header_level}>"
+        for cidx, char in enumerate(line):
+            if char == '[':  # Start of Link
+                link_text = line[cidx+1:]
+                end_idx = link_text.index(']')
+                link_text = link_text[:end_idx]
+                print(link_text)
+
+
+
+
+
+    html_content = lines
+    return render(request, "encyclopedia/layout.html", {
+        "markdown": html_content
     })
+
 
